afd2d7b5be53a72008bfa1292ff5fc6963606404
adrianycmc <adrianycmc@outlook.com>
2024-07-15 17:28:21 -0300

    aplicando o conceito de herança

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from modelos.restaurante import Restaurante
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('El Mexiano', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-
-restaurante_mexicano.alternar_estado()
-
-restaurante_praca.receber_avaliacao('Adriany', 4)
-restaurante_praca.receber_avaliacao('Mayara', 6.7)
+bebida_suco = Bebida ('Suco de Cajú', 5.0, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.0, 'Pão doce especial da casa')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
---
+++ modelos/cardapio/item_cardapio.py
@@ -0,0 +1,5 @@
+class ItemCardapio:
+    def __init__(self, nome, preco):
+        self._nome = nome
+        self.preco = preco
+        
